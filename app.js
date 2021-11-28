cb957205cbbffb075059aadf04ee6d9e34022dfc
yankie <yankielarapacheco@yahoo.com>
2021-11-28 11:48:42 -0500

    Road trip website Use hbs engine

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+// Engine hbs
+app.set('view engine', 'hbs')
+
+// Static content
 app.use(express.static('public'))
 
 app.get('/generic', (req, res) => {
