6326911cb0b3c709aa6ad1dd3751b6692f95cc92
yankie <yankielarapacheco@yahoo.com>
2021-11-28 11:33:43 -0500

    Road trip app

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const port = 8080
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.use(express.static('public'))
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
+})
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
 })
 
-app.listen(8080)
+app.listen(port, () => {
+    console.log(`App is listening in http://localhost:${port}`)
+})
