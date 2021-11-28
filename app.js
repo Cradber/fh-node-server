a5cf7c45a2ede24841be30b31d899765c37d75e4
yankie <yankielarapacheco@yahoo.com>
2021-11-28 10:22:37 -0500

    Created a basic server

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 
-console.log('Hello world')
+const http = require('http')
+
+http.createServer((req, res) => {
+    res.write('Hello world')
+    res.end()
+})
+.listen(8080)
+
+console.log('Listening at port 8080')
